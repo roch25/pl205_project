e2a3a9083bb5044b4a55f286f81a84c295447c94
git-roch <52605935+git-roch@users.noreply.github.com>
2020-03-31 20:26:05 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /* PL205 project
-  Authors: 1904   Dalvi
-                  1929   Chetana Marathe
-                  1930.  Rochelle Martins
+  Authors: 1904 Shruti  Dalvi
+           1929   Chetana Marathe
+           1930  Rochelle Martins
  Description: Phone directory using Trie data structure.
 */
 int main()
