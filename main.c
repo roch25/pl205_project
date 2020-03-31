e27761c8e1845371cc3d1edc2ea8ac0802ea76ce
chetana1929 <62947769+chetana1929@users.noreply.github.com>
2020-03-31 20:22:30 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /* PL205 project
-  Authors: Shruti Dalvi
-           Chetana Marathe
-           Rochelle Martins
+  Authors: 1904   Dalvi
+                  1929   Chetana Marathe
+                  1930.  Rochelle Martins
  Description: Phone directory using Trie data structure.
 */
 int main()
