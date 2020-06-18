d3a87520d319618de3965ed6a7d24c5977d9aeb9
git-roch <rochellemartins25@gmail.com>
2020-06-19 03:05:35 +0530

    created contact data structure

--- phone_directory.h
+++ phone_directory.h
@@ -1,7 +1,15 @@
 // header fill
+
 struct trie
 {
   char value;
   struct trie *next[26];
 };
 typedef struct trie trie;
+
+struct contact
+{
+  trie name;
+  int phone_number[3][12];
+};
+typedef struct contact contact;
