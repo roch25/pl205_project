7fa1b684faea8ca2b4e38806d561cfefd5a4cf37
dalvishruti <62990454+dalvishruti@users.noreply.github.com>
2020-06-20 15:16:20 +0530

    Update phoneDirectory.c

--- phoneDirectory.c
+++ phoneDirectory.c
@@ -7,21 +7,42 @@
 
 int add_contact()
 {
-  int op;
+  int op,int flag=0;
   contact *new_contact;
   new_contact = (contact*)malloc(sizeof(contact));
   if(new_contact != NULL)
   {
     // to-do: name
+    while(flag==0)
+    {
     // phone number
     printf("Enter phone number ");
     scanf("%s", new_contact->phone_number[0]);
     
     //validation function call
-    validate_phone_number(new_contact->phone_number);
+    flag=validate_phone_number(new_contact->phone_number);
+     if(flag==0)
+     {
+         printf("INVALID PHONE NUMBER. PLEASE ENTER A VALID PHONE NUMBER\n");
+     }
+    }
   }
   else
   {
     return -1; // memory could not be allocated
+  }
+}
+
+int validate_phone_number(char value[])
+{
+  int length;
+  length=strlen(value);
+  if(length != 10)
+  {
+    return 0;
+  }
+  else
+  {
+      return 1;
   }
 }
