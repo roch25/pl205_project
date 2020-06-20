29e97e9b5e16d11b23b06450eed20d6464e1c393
git-roch <rochellemartins25@gmail.com>
2020-06-20 14:41:39 +0530

    add phone number temp

--- phoneDirectory.c
+++ phoneDirectory.c
@@ -3,22 +3,23 @@
 #include "phoneDirectory.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int add_contact()
 {
   int op;
   contact *new_contact;
-  //allocate space ...
   new_contact = (contact*)malloc(sizeof(contact));
   if(new_contact != NULL)
   {
-    printf("Enter phone number");
+    // to-do: name
+    // phone number
+    printf("Enter phone number ");
     scanf("%s", new_contact->phone_number[0]);
+    //validation function call
   }
   else
   {
-    op = -1; // memory could not be allocated
+    return -1; // memory could not be allocated
   }
-  printf("%s",  new_contact->phone_number[0]);
-
 }
