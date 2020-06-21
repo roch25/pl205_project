4984b663e8b168d44163aba2d481dca1e26ad727
git-roch <rochellemartins25@gmail.com>
2020-06-21 13:49:08 +0530

    minor fixes

--- phoneDirectory.c
+++ phoneDirectory.c
@@ -21,44 +21,37 @@ int validate_name(char name[25])
   }
   return 0; //if valid
 }
+
 int add_contact()
 {
   int op, flag = 0;
   contact *new_contact;
-  // contact *c_name; //(contact name)
   new_contact = (contact *)malloc(sizeof(contact));
-  // c_name=(contact*)malloc(strlen(c_name)*sizeof(contact));
-
   if (new_contact != NULL)
   {
-    // to-do: name
     int c;
     char name[25];
     printf("\n Enter name ");
     scanf("%s", name);
-
-    //validation function call
     c = validate_name(name);
     if (c == 1)
       printf("\n Length of name too small. Please enter your name again\n ");
     else if (c == 2)
       printf("\n Name should not contain any numbers or special chracters other than an apostrophe(') and a full point(.)\n ");
-    else
-    { // name is valid
-      strcpy(new_contact->name, name);
+    else // name is valid
+    {
+      // strcpy(new_contact->name, name);
+      /// ++++++
     }
 
-    {
-      // phone number
-      printf("Enter phone number ");
-      scanf("%s", new_contact->phone_number[0]);
+    printf("Enter phone number ");
+    scanf("%s", new_contact->phone_number[0]);
 
-      //validation function call
-      flag = validate_phone_number(new_contact->phone_number);
-      if (flag == 0)
-      {
-        printf("INVALID PHONE NUMBER. PLEASE ENTER A VALID 10 DIGITS PHONE NUMBER\n");
-      }
+    //validation function call
+    flag = validate_phone_number(new_contact->phone_number[0]);
+    if (flag == 0)
+    {
+      printf("INVALID PHONE NUMBER. PLEASE ENTER A VALID 10 DIGITS PHONE NUMBER\n");
     }
   }
   else
