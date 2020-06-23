f49119082e5300bbc02caa1bf1f26b6397d83e05
git-roch <rochellemartins25@gmail.com>
2020-06-23 10:38:32 +0530

    Improvs, validation check

--- phoneDirectory.c
+++ phoneDirectory.c
@@ -5,19 +5,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#define VALID(x) ((x > 64 && x < 91) || (x > 96 && x < 123) || x == 39 || x == 32 || x == 46)
-//65-90(A-Z) 97-122(a-z) 32(space) 39(') 46(.)
+#define IS_UPPER_CASE(x) (x > 64 && x < 91)
+#define IS_LOWER_CASE(x) (x > 96 && x < 123)
+#define IS_APOSTROPHE(x) x == 39
+#define IS_SPACE(x) x == 32
+#define IS_FULLPOINT(x) x == 46
 
 int validate_name(char name[25])
 {
   int i = 0, c;
-  if (strlen(name) < 3)
-    return 1; //if length is too short
-  while (i < strlen(name))
+  char ch;
+  if (strlen(name) < 3) // length validation
+    return 1;
+  ch = name[0];
+  if (IS_SPACE(ch) || IS_APOSTROPHE(ch) || IS_FULLPOINT(ch)) // first character validation
+    return 2;
+  while (i < strlen(name)) //if it contains numbers or special charcters other than a full point or apostrophe
   {
-    c = VALID(name[i]);
+    ch = name[i];
+    c = IS_UPPER_CASE(ch) || IS_LOWER_CASE(ch) || IS_SPACE(ch) || IS_APOSTROPHE(ch) || IS_FULLPOINT(ch);
     if (!c)
-      return 2; //if it contains numbers or special charcters other than a full point or apostrophe
+      return 3;
     i++;
   }
   return 0; //if valid
@@ -35,9 +43,20 @@ int add_contact()
     printf("\nEnter name ");
     fseek(stdin, 0, SEEK_END);
     scanf(" %[^\n]", name);
-    while (validate_name(name))
+    int x;
+    while (x = validate_name(name))
     {
-      printf("Invalid name. Length of name should be at least 3 characters and should not contain any numbers or special chracters other than an apostrophe(') and a full point(.) Please enter the name again\n");
+      switch (x)
+      {
+      case 1:
+        printf("Length of name should be at least 3 characters");
+      case 2:
+        printf("Name should not begin with a apostrophe or a full point");
+        break;
+      case 3:
+        printf("Name should not contain any numbers or special chracters other than an apostrophe(') and a full point(.) ");
+      }
+      printf("Please enter the name again\n");
       scanf("%s", name);
     }
 
@@ -84,24 +103,23 @@ int validate_phone_number(char value[])
         return 0;
       }
     }
-    	 check_phone_number(value);
-	 return 1;
+    // check_if_phone_number_exists(value);
+    return 1;
   }
 }
 
-int write_to_file(contact *new) //function to write phone number to a file
+void write_to_file(contact *new) //function to write phone number to a file
 {
   FILE *fp;
-  fp = fopen("phone_number.txt", "a+");
+  fp = fopen("phone_number.dat", "ab");
   if (fp == NULL)
   {
     printf("Cannot open file\n  ");
-    exit(0);
+    return;
   }
-  else
-  {
-    fprintf(fp, "%s\n", new->phone_number[0]);
-  }
+  int record_added = fwrite(&new, sizeof(new), 1, fp);
+  if (record_added)
+    printf("\n\n Contact details added succesfully!\n");
   fclose(fp);
 }
 
@@ -111,17 +129,6 @@ int insert(trie *root, char *name)
   int sub;
   while (*name) //iterate over each character in the name to insert in the trie
   {
-    // if (*name > 65 && *name < 92)
-    //   sub = 70;
-    // else if (*name > 65 && *name < 92)
-    //   sub = 65;
-    // else if (*name == 32)
-    //   sub = -21; // map to 53
-    // else if (*name == 39)
-    //   sub = -14;
-    // else
-    //   sub = -9;
-    // next line is equivalent to the commented code above
     sub = (*name > 64 && *name < 92) ? 65 : ((*name > 96 && *name < 123) ? 70 : ((*name == 32) ? -21 : (*name == 39) ? -15 : -9));
     if (curr->next[*name - sub] == NULL)
       curr->next[*name - sub] = (trie *)malloc(sizeof(trie)); //set the node
@@ -134,35 +141,19 @@ int insert(trie *root, char *name)
   curr->isLeaf = 1; // indicates end
 }
 
-int check_phone_number(contact *new)            //function to check if phone number is identical
+int check_phone_number(char *phone_num) //function to check if phone number is identical
 {
-	int flag=0;
-	FILE *fp1;
-	fp1 = fopen("phone_number.txt","r");
-	if (fp1 == NULL)
-	{
-	  printf("Cannot open file\n");
-	  exit(0);
-	}
-	else
-	{
-	int val;
-	while(fscanf(fp1,"%s \n",new->phone_number[0])!=EOF)
-	{
-	val=strcmp(new->phone_number[0],new);
-		if(val==0)
-		{	 printf("PHONE NUMBER ALREADY EXIST. PLEASE ENTER A VALID PHONE NUMBER\n");
-			while (flag == 0)
-			{
-			     
-			      scanf("%s", new->phone_number[0]);
-			      flag = validate_phone_number(new->phone_number[0]);
-			      write_to_file(new);
-			}
-		}
-		
-	}
-	}
+  int flag = 0;
+  FILE *fp1;
+  fp1 = fopen("phone_number.txt", "r");
+  if (fp1 == NULL)
+  {
+    printf("Cannot open file\n");
+    exit(0);
+  }
+  else
+  {
+  }
 
-	fclose(fp1);
+  fclose(fp1);
 }
